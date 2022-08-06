a07af74ba10dd117b16142c48441a2e3f58098ea
Likith P S <likithps121@gmail.com>
2022-08-06 19:44:58 +0530

    Changed app.listen

--- App.js
+++ App.js
@@ -106,6 +106,5 @@ app.delete('/song/:id', (req,res) =>{
     }
     res.sendStatus(200)
 })
-app.listen(3000, () => {
-    console.log('Listening to port 3000')
-})
+
+app.listen(process.env.PORT || 3000)
